73449d8496b368522c7555788a170112b0d71ffe
Manuel Valls Fernández <manolo@vlrz.es>
2016-04-13 20:22:25 +0200

    Fix junit format

--- main/print/junit.js
+++ main/print/junit.js
@@ -21,17 +21,17 @@ module.exports.after = function(options){
 
     t += (tree.t1 - tree.t0) / 1000;
 
-    if(!tree.error) ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'\\"')}" time="${(tree.t1 - tree.t0)/1000}" />${syntax.getNL(options.syntax)}`;
+    if(!tree.error) ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'&quot;')}" time="${(tree.t1 - tree.t0)/1000}" />${syntax.getNL(options.syntax)}`;
     else{
       if(tree.error.name == 'AssertionError'){
         failures++;
-        ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'\\"')}" time="${(tree.t1 - tree.t0)/1000}">${syntax.getNL(options.syntax)}`;
-        ret += `    <failure message="${tree.error.message.replace(/&/g,'&amp;').replace(/"/g,'\\"')}">${tree.error.stack.replace(/&/g,'&amp;').replace('<','&lt;').replace('>','&gt;')}</failure>${syntax.getNL(options.syntax)}`;
+        ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'&quot;')}" time="${(tree.t1 - tree.t0)/1000}">${syntax.getNL(options.syntax)}`;
+        ret += `    <failure message="${tree.error.message.replace(/&/g,'&amp;').replace(/"/g,'&quot;')}">${tree.error.stack.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')}</failure>${syntax.getNL(options.syntax)}`;
         ret += `  </testcase>${syntax.getNL(options.syntax)}`;
       }else{
         errors++;
-        ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'\\"')}" time="${(tree.t1 - tree.t0)/1000}">${syntax.getNL(options.syntax)}`;
-        ret += `    <error message="${tree.error.message.replace(/&/g,'&amp;').replace(/"/g,'\\"')}">${tree.error.stack.replace(/&/g,'&amp;').replace('<','&lt;').replace('>','&gt;')}</error>${syntax.getNL(options.syntax)}`
+        ret += `  <testcase classname="" name="${tree.info.replace(/&/g,'&amp;').replace(/"/g,'&quot;')}" time="${(tree.t1 - tree.t0)/1000}">${syntax.getNL(options.syntax)}`;
+        ret += `    <error message="${tree.error.message.replace(/&/g,'&amp;').replace(/"/g,'&quot;')}">${tree.error.stack.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')}</error>${syntax.getNL(options.syntax)}`
         ret += `  </testcase>${syntax.getNL(options.syntax)}`;
       }
     }
